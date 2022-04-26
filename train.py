52652adf197c5a3cf23d54cdb878c666e614bb61
sublatelog <blueganz@gmail.com>
2022-04-26 17:08:35 +0900

    Update train.py

--- train.py
+++ train.py
@@ -8,83 +8,30 @@ from disk.loss import Reinforce, DepthReward, EpipolarReward, \
                       PoseQuality, DiscreteMetric
 
 parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
-parser.add_argument(
-    'data_path', type=str,
-    help=('Path to the datasets. This should point to the location with '
-          '`megadepth` and `imw2020-val` directories.'),
-)
-parser.add_argument(
-    '--reward', choices=['epipolar', 'depth'], default='depth',
-    help='Reward criterion to use'
-)
-parser.add_argument(
-    '--save-dir', type=str, default='artifacts',
-    help=('Path for saving artifacts (checkpoints and tensorboard logs). Will '
-          'be created if doesn\'t exist')
-)
-parser.add_argument(
-    '--batch-size', type=int, default=2,
-    help='The size of the batch',
-)
-parser.add_argument(
-    '--chunk-size', type=int, default=5000,
-    help=('The number of batches in the (pseudo) epoch. We run validation and '
-          'save a checkpoint once per epoch, as well as use them for scheduling'
-          ' the reward annealing'),
-)
-parser.add_argument(
-    '--substep', type=int, default=1,
-    help=('Number of batches to accumulate gradients over. Can be increased to'
-          ' compensate for smaller batches on GPUs with less VRAM'),
-)
-parser.add_argument(
-    '--warmup', type=int, default=250,
-    help=('The first (pseudo) epoch can be much shorter, this avoids wasting '
-          'time.'),
-)
-parser.add_argument(
-    '--height', type=int, default=768,
-    help='We train on images resized to (height, width)',
-)
-parser.add_argument(
-    '--width', type=int, default=768,
-    help='We train on images resized to (height, width)',
-)
-parser.add_argument(
-    '--train-scene-limit', type=int, default=1000,
-    help=('Different scenes in the dataset have a different amount of '
-          'covisible image triplets. We (randomly) subselect '
-          '--train-scene-limit of them for training, to avoid introducing '
-          'a data bias towards those scenes.')
-)
-parser.add_argument(
-    '--test-scene-limit', type=int, default=250,
-    help=('Different scenes in the dataset have a different amount of '
-          'covisible image triplets. We (randomly) subselect '
-          '--test-scene-limit of them for validation to avoid '
-          'to avoid introducing a bias towards those scenes.')
-)
-parser.add_argument(
-    '--n-epochs', type=int, default=50,
-    help='Number of (pseudo) epochs to train for',
-)
-parser.add_argument(
-    '--desc-dim', type=int, default=128,
-    help='Dimensionality of descriptors to produce. 128 by default',
-)
-parser.add_argument(
-    '--load', type=str, default=None,
-    help='Path to a checkpoint to resume training from',
-)
-parser.add_argument(
-    '--epoch-offset', type=int, default=0,
-    help=('Start counting epochs from this value. Influences the annealing '
-          'procedures, and is therefore useful when restarting from a '
-          'checkpoint'),
-)
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter
+                                )
+parser.add_argument('data_path', type=str, help=('Path to the datasets. This should point to the location with `megadepth` and `imw2020-val` directories.'),)
+parser.add_argument('--reward', choices=['epipolar', 'depth'], default='depth', help='Reward criterion to use')
+parser.add_argument('--save-dir', type=str, default='artifacts', help=('Path for saving artifacts (checkpoints and tensorboard logs). Will be created if doesn\' t exist'))
+parser.add_argument('--batch-size', type=int, default=2, help='The size of the batch',)
+parser.add_argument('--chunk-size', type=int, default=5000, help=('The number of batches in the (pseudo) epoch. We run validation and save a checkpoint once per epoch, as well as use them for scheduling the reward annealing'),)
+parser.add_argument('--substep', type=int, default=1,help=('Number of batches to accumulate gradients over. Can be increased to compensate for smaller batches on GPUs with less VRAM'),)
+parser.add_argument('--warmup', type=int, default=250, help=('The first (pseudo) epoch can be much shorter, this avoids wasting time.'),)
+parser.add_argument('--height', type=int, default=768, help='We train on images resized to (height, width)',)
+parser.add_argument('--width', type=int, default=768, help='We train on images resized to (height, width)',)
+parser.add_argument('--train-scene-limit', type=int, default=1000, help=('Different scenes in the dataset have a different amount of covisible image triplets. '
+                                                                         'We (randomly) subselect --train-scene-limit of them for training, to avoid introducing a data bias towards those scenes.'))
+parser.add_argument('--test-scene-limit', type=int, default=250, help=('Different scenes in the dataset have a different amount of covisible'
+                                                                      ' image triplets. We (randomly) subselect --test-scene-limit of them for validation to avoid '
+                                                                      'to avoid introducing a bias towards those scenes.'))
+parser.add_argument('--n-epochs', type=int, default=50, help='Number of (pseudo) epochs to train for',)
+parser.add_argument('--desc-dim', type=int, default=128, help='Dimensionality of descriptors to produce. 128 by default',)
+parser.add_argument('--load', type=str, default=None, help='Path to a checkpoint to resume training from',)
+parser.add_argument('--epoch-offset', type=int, default=0, help=('Start counting epochs from this value. Influences the annealing '
+                                                                'procedures, and is therefore useful when restarting from a checkpoint'),)
 args = parser.parse_args()
+
+
 
 DEV  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {DEV}')
@@ -107,18 +54,18 @@ disk = disk.to(DEV)
 # camera position and intrinsics (focal length, etc) and optionally
 # depth maps.
 train_chunk_iter, test_iter = get_datasets(
-    args.data_path,
-    no_depth=args.reward == 'epipolar',
-    batch_size=args.batch_size,
-    chunk_size=args.chunk_size,
-    substep=args.substep,
-    n_epochs=args.n_epochs,
-    train_limit=args.train_scene_limit,
-    test_limit=args.test_scene_limit,
+                                          args.data_path,
+                                          no_depth=args.reward == 'epipolar',
+                                          batch_size=args.batch_size,
+                                          chunk_size=args.chunk_size,
+                                          substep=args.substep,
+                                          n_epochs=args.n_epochs,
+                                          train_limit=args.train_scene_limit,
+                                          test_limit=args.test_scene_limit,
+                                          crop_size=(args.height, args.width),
+                                          )
 
-    crop_size=(args.height, args.width),
-)
-
+# logger
 logger = Logger(args.save_dir)
 
 # set up the inference-time matching algorthim and validation metrics
@@ -126,14 +73,17 @@ valtime_matcher     = CycleMatcher()
 pose_quality_metric = PoseQuality()
 disc_quality_metric = DiscreteMetric(th=1.5, lm_kp=-0.01)
 
+
 if args.reward == 'epipolar':
     reward_class = EpipolarReward
 elif args.reward == 'depth':
     reward_class = DepthReward
 else:
     raise ValueError(f'Unknown reward mode `{args.reward}`')
+    
 
 optim = torch.optim.Adam(disk.parameters(), lr=1e-4)
+
 
 for e, chunk in enumerate(train_chunk_iter):
     # this allows us to offset the annealing below, for instance when resuming
@@ -170,7 +120,7 @@ for e, chunk in enumerate(train_chunk_iter):
     matcher = ConsistentMatcher(inverse_T=inverse_T).to(DEV)
     matcher.requires_grad_(False)
 
-    # the main training loop
+    # the main training loop ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
     for i, batch in enumerate(tqdm(chunk, total=args.chunk_size)):
         # get the data onto GPU
         bitmaps, images = batch.to(DEV, non_blocking=True)
@@ -183,6 +133,7 @@ for e, chunk in enumerate(train_chunk_iter):
         # extract the features. They are a numpy array of size [2 * batch_size]
         # which contains objects of type disk.common.Features
         features_ = disk.features(bitmaps_, kind='rng')
+        
         # reshape them back to [2, batch_size]
         features = features_.reshape(*bitmaps.shape[:2])
 
@@ -216,7 +167,7 @@ for e, chunk in enumerate(train_chunk_iter):
         'disk': disk.state_dict(),
     }, f'{args.save_dir}/save-{e}.pth')
 
-    # validation loop
+    # validation loop ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
     for i, batch in enumerate(tqdm(test_iter)):
         bitmaps, images = batch.to(DEV, non_blocking=True)
         bitmaps_ = bitmaps.reshape(-1, *bitmaps.shape[2:])
