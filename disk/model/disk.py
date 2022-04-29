34c952f04cce96ebb02fee26f6212491f32e6e14
sublatelog <blueganz@gmail.com>
2022-04-29 16:17:59 +0900

    Update disk.py

--- disk/model/disk.py
+++ disk/model/disk.py
@@ -11,31 +11,36 @@ DEFAULT_SETUP = {**thin_setup, 'bias': True, 'padding': True}
 
 class DISK(torch.nn.Module):
     def __init__(
-        self,
-        desc_dim=128,
-        window=8,
-        setup=DEFAULT_SETUP,
-        kernel_size=5,
-    ):
+                self,
+                desc_dim=128,
+                window=8,
+                setup=DEFAULT_SETUP,
+                kernel_size=5,
+                ):
+        
         super(DISK, self).__init__()
 
-        self.desc_dim = desc_dim
+        self.desc_dim = desc_dim # 128
+        
         self.unet = Unet(
-            in_features=3, size=kernel_size,
-            down=[16, 32, 64, 64, 64],
-            up=[64, 64, 64, desc_dim+1],
-            setup=setup,
-        )
+                        in_features=3, 
+                        size=kernel_size,
+                        down=[16, 32, 64, 64, 64],
+                        up=[64, 64, 64, desc_dim+1],
+                        setup=setup, # {**thin_setup, 'bias': True, 'padding': True}
+                        )
+        
         self.detector = Detector(window=window)
+        
 
     @dimchecked
-    def _split(self, unet_output: ['B', 'C', 'H', 'W']) \
-                -> (['B', 'C-1', 'H', 'W'], ['B', 1, 'H', 'W']):
+    def _split(self, unet_output: ['B', 'C', 'H', 'W']) -> (['B', 'C-1', 'H', 'W'], ['B', 1, 'H', 'W']):
         '''
         Splits the raw Unet output into descriptors and detection heatmap.
         '''
         assert unet_output.shape[1] == self.desc_dim + 1
 
+        # descriptorsとheatmapに分割
         descriptors = unet_output[:, :self.desc_dim]
         heatmap     = unet_output[:, self.desc_dim:]
 
@@ -43,17 +48,20 @@ class DISK(torch.nn.Module):
 
     @dimchecked
     def features(
-        self,
-        images: ['B', 'C', 'H', 'W'],
-        kind='rng',
-        **kwargs
-    ) -> NpArray[Features]:
-        ''' allowed values for `kind`:
+                self,
+                images: ['B', 'C', 'H', 'W'],
+                kind='rng',
+                **kwargs
+                ) -> NpArray[Features]:
+        
+        ''' 
+        allowed values for `kind`:
             * rng
             * nms
         '''
 
         B = images.shape[0]
+        
         try:
             descriptors, heatmaps = self._split(self.unet(images))
         except RuntimeError as e:
@@ -66,11 +74,14 @@ class DISK(torch.nn.Module):
             else:
                 raise
 
+        # 'rng': keypoints = self.detector.sample(heatmaps, **kwargs)
         keypoints = {
             'rng': self.detector.sample,
             'nms': self.detector.nms,
         }[kind](heatmaps, **kwargs)
+        
 
+        # keypointとdescriptorの結合
         features = []
         for i in range(B):
             features.append(keypoints[i].merge_with_descriptors(descriptors[i]))
